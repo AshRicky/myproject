a9353d478da97835fe6574ca8a930f4812054616
AshRicky <ashishricky2004@gmail.com>
2025-03-31 01:44:21 +0530

    Initial Files

---
+++ backend/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class ItemRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+# Create your views here.
